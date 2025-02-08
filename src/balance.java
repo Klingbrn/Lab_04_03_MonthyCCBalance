5f89eeff0581ee651add797e6a6721f00eac63b9
klingbrn <klingbrn@mail.uc.edu>
2025-02-07 20:33:44 -0500

    Final commit

---
+++ src/balance.java
@@ -0,0 +1,16 @@
+public class balance {
+    public static void main(String[] args) {
+
+        int cardBalance = 5000;
+        double INTEREST_RATE = 0.17;
+        double firstMonth;
+        double secondMonth;
+
+        firstMonth = (cardBalance * INTEREST_RATE) + cardBalance;
+        secondMonth = (firstMonth * INTEREST_RATE) + firstMonth;
+
+        System.out.println("your firt month balance is: " + firstMonth);
+        System.out.println("you second month balance is: " + secondMonth);
+
+        }
+    }
